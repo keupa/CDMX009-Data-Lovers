db08c29c28962ac1cb9ce71b05997d0bc0171777
unknown <alitasang@gmail.com>
2020-03-10 13:06:12 -0600

    lista años

--- src/data.js
+++ src/data.js
@@ -1,121 +1,121 @@
 
-//nodes
+// //nodes
    
-let select = document.querySelector('.select')
-let selectI = document.querySelector('#indicator')
-let h3 = document.querySelector('#result')
+// let select = document.querySelector('.select')
+// let selectI = document.querySelector('#indicator')
+// let h3 = document.querySelector('#result')
 
 
-fetch('./data/worldbank/worldbank.json')
-.then(response=>response.json())
-.then(data=>(data.PER.indicators["17"].data["1990"]));
+// fetch('./data/worldbank/worldbank.json')
+// .then(response=>response.json())
+// .then(data=>(data.PER.indicators["17"].data["1990"]));
 
+// // // .then(data=>console.log(data.PER.indicators["17"].indicatorName ));
+// //    .then(( data)=>{
+// //        for (let i=0; i <= 138; i++) {
+// //         let index="";
+// //             //  index += console.log(data.PER.indicators[i].indicatorName );             
+// //            //  index += document.getElementById("text-area").innerHTML+=
+// //            document.getElementById("textArea").innerHTML+=
+// //              (data.PER.indicators[i].indicatorName );
+            
+               
+// //      }
+// //     }
+// //    )
+
+// fetch('./data/worldbank/worldbank.json')
+// .then(response=>response.json())
+//    .then(( data)=>{
+      
+//         let index1="";
+//              index1= (data.PER.indicators["17"].data); 
+//              console.log(Object.keys(index1));              
+           
+//     }
+//    )
+
+//    fetch('./data/worldbank/worldbank.json')
+// .then(response=>response.json())
+//    .then(( data)=>{
+      
+//         let index="";
+//              index = (data); 
+//              console.log(Object.keys(index));              
+           
+//     }
+//    )
+
+
+//    fetch('./data/worldbank/worldbank.json')
+// .then(response=>response.json())
+// // .then(data=>console.log(data.PER.indicators["17"].data["1990"]));
 // // .then(data=>console.log(data.PER.indicators["17"].indicatorName ));
 //    .then(( data)=>{
 //        for (let i=0; i <= 138; i++) {
 //         let index="";
-//             //  index += console.log(data.PER.indicators[i].indicatorName );             
-//            //  index += document.getElementById("text-area").innerHTML+=
-//            document.getElementById("textArea").innerHTML+=
-//              (data.PER.indicators[i].indicatorName );
-            
+//             //  index += console.log(data.PER.indicators[i].indicatorName ); 
+//              index = (data.PER.indicators[i].indicatorName ); 
+//              document.getElementById("indicator").innerHTML+=("<option>" + index + "<option>");            
+// //             index += document.getElementById("textArea").innerHTML+=
+// //            document.getElementById("text-area").innerHTML+=
+// //              (data.PER.indicators[i].indicatorName );
+           
                
 //      }
 //     }
 //    )
-
-fetch('./data/worldbank/worldbank.json')
-.then(response=>response.json())
-   .then(( data)=>{
-      
-        let index1="";
-             index1= (data.PER.indicators["17"].data); 
-             console.log(Object.keys(index1));              
-           
-    }
-   )
-
-   fetch('./data/worldbank/worldbank.json')
-.then(response=>response.json())
-   .then(( data)=>{
-      
-        let index="";
-             index = (data); 
-             console.log(Object.keys(index));              
-           
-    }
-   )
-
-
-   fetch('./data/worldbank/worldbank.json')
-.then(response=>response.json())
-// .then(data=>console.log(data.PER.indicators["17"].data["1990"]));
-// .then(data=>console.log(data.PER.indicators["17"].indicatorName ));
-   .then(( data)=>{
-       for (let i=0; i <= 138; i++) {
-        let index="";
-            //  index += console.log(data.PER.indicators[i].indicatorName ); 
-             index = (data.PER.indicators[i].indicatorName ); 
-             document.getElementById("indicator").innerHTML+=("<option>" + index + "<option>");            
-//             index += document.getElementById("textArea").innerHTML+=
-//            document.getElementById("text-area").innerHTML+=
-//              (data.PER.indicators[i].indicatorName );
-           
-               
-     }
-    }
-   )
 
 
 
  
   
 
-  fetch('./data/worldbank/worldbank.json')
-  .then(response=>response.json())
-  .then(data=>console.log(data.PER.indicators["17"].data["1990"]));
-  // // .then(data=>console.log(data.PER.indicators["17"].indicatorName ));
-  //    .then(( data)=>{
-  //        for (let x=0; x <= 57; x++) {
-  //         let year= 1960;
-  //         document.getElementById("year").innerHTML+=("<option>" + year + "<option>"); 
+//   fetch('./data/worldbank/worldbank.json')
+//   .then(response=>response.json())
+//   .then(data=>console.log(data.PER.indicators["17"].data["1990"]));
+//   // // .then(data=>console.log(data.PER.indicators["17"].indicatorName ));
+//   //    .then(( data)=>{
+//   //        for (let x=0; x <= 57; x++) {
+//   //         let year= 1960;
+//   //         document.getElementById("year").innerHTML+=("<option>" + year + "<option>"); 
          
                
-  //              console.log(year+x);            
+//   //              console.log(year+x);            
                           
-    //               }
-    //   }
-    //  )
+//     //               }
+//     //   }
+//     //  )
 
-      // fetch('./data/worldbank/worldbank.json')
-      // .then(response=>response.json())
-      // .then(data=>console.log(data.PER.indicators[]data["1990"]));
+//       // fetch('./data/worldbank/worldbank.json')
+//       // .then(response=>response.json())
+//       // .then(data=>console.log(data.PER.indicators[]data["1990"]));
 
-   function genera_tabla() {
-       var body = document.getElementsByTagName("textArea")[0];
-        var tabla   = document.createElement("table");
-    var tblBody = document.createElement("tbody");
+//    function genera_tabla() {
+//        var body = document.getElementsByTagName("textArea")[0];
+//         var tabla   = document.createElement("table");
+//     var tblBody = document.createElement("tbody");
    
-    for (let ind = 0; ind < 13; ind++) {
-         var hilera = document.createElement("tr");
+//     for (let ind = 0; ind < 13; ind++) {
+//          var hilera = document.createElement("tr");
    
-      for (var j = 0; j < 2; j++) {
-            var celda = document.createElement("td");
-        var textoCelda = document.createTextNode("celda en la hilera  columna "+j);
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-      }
+//       for (var j = 0; j < 2; j++) {
+//             var celda = document.createElement("td");
+//         var textoCelda = document.createTextNode("celda en la hilera  columna "+j);
+//         celda.appendChild(textoCelda);
+//         hilera.appendChild(celda);
+//       }
    
-        tblBody.appendChild(hilera);
-    }
+//         tblBody.appendChild(hilera);
+//     }
    
     
-    tabla.appendChild(tblBody);
+//     tabla.appendChild(tblBody);
    
-    body.appendChild(tabla);
-    tabla.setAttribute("border", "2");
+//     body.appendChild(tabla);
+//     tabla.setAttribute("border", "2");
     
-  }
+//   }
 
 
 
@@ -126,7 +126,7 @@ fetch('./data/worldbank/worldbank.json')
 // fetch('./data/worldbank/worldbank.json')
 // .then(response=>response.json())
 
-// // .then(data=>console.log(data.PER.indicators["17"]));
+// .then(data=>console.log(data.PER.indicators["17"]));
 
 // let datos = JSON.parse(this.responseText);
 // for (let item of datos){
@@ -146,12 +146,12 @@ fetch('./data/worldbank/worldbank.json')
 //     })
 //     .then(function(data){
 //         console.log(data.PER.indicators['17']);
-//         // let html="";
-//         // data.forEach(function(pais) {
-//         //    html += "<li>${data.PER} </li> ";
-//         // })
-//         //     document.getElementById("resultado").innerHTML=html;
-//     })
+//     //     // let html="";
+//     //     // data.forEach(function(pais) {
+//     //     //    html += "<li>${data.PER} </li> ";
+//     //     // })
+//     //     //     document.getElementById("resultado").innerHTML=html;
+//     // })
 // }
 // output+=
 // '
@@ -174,25 +174,25 @@ fetch('./data/worldbank/worldbank.json')
 //     });
 
 // }
-select.addEventListener('change', e=>{
-  let value = e.target.value
-  h3.innerText = value
-  let country=value
+// select.addEventListener('change', e=>{
+//   let value = e.target.value
+//   h3.innerText = value
+//   let country=value
 
-  fetch('./data/worldbank/worldbank.json')
-  .then(response=>response.json())
-  .then(data=>console.log((data[country].indicators["17"].data["1990"])));
+//   fetch('./data/worldbank/worldbank.json')
+//   .then(response=>response.json())
+//   .then(data=>console.log((data[country].indicators["17"].data["1990"])));
     
-})
+// })
 
-selectI.addEventListener('change', e=>{
-  let valueI = e.target.value
-  h3.innerText = valueI
-  let indic=valueI
+// selectI.addEventListener('change', e=>{
+//   let valueI = e.target.value
+//   h3.innerText = valueI
+//   let indic=valueI
 
-  fetch('./data/worldbank/worldbank.json')
-  .then(response=>response.json())
-  .then(data=>console.log((data[country].indicators[indic].data["1990"])));
+//   fetch('./data/worldbank/worldbank.json')
+//   .then(response=>response.json())
+//   .then(data=>console.log((data[country].indicators[indic].data["1990"])));
     
-})
+// })
  
--- src/main.js
+++ src/main.js
@@ -1,6 +1,64 @@
-import {fetchData} from './data.js';
-var placeBtn = document.querySelector('.place-icon');
-placeBtn.addEventListener('click',
-fetchData());
+// 1.- importamos la Data
+import data from './data/worldbank/worldbank.js'
+
+//5.- Guardamos en variables globales
+let country
+let val=0
+// let indi2
+// 2.- selectores (nodos)
+let select = document.querySelector('#cText')
+let indiSelect = document.querySelector('#indicator')
+let yearSelect = document.querySelector('#year')
+
+//4.- funciones callback
+function onChange(event){
+  let value = event.target.value
+  country = value
+  let indi = data[value].indicators
+  console.log(indi)
+  setOptions(indi)
+}
+
+function onChange1(event1){
+  let value1 = event1.target.value
+  let indicator = value1
+  console.log(indicator)
+    // indiucador hay filtrar con ase al indicador
+  let indicador = data[country].indicators.find(obj=>obj.indicatorName===indicator)
+  setOptionsY(Object.keys(indicador.data))
+  console.log(Object.values(indicador.data))
+
+}
 
 
+
+
+
+//6.- funcion para colocar options en el select de indicator
+function setOptions(list){
+  list.forEach(i=>{
+    //a.- creamos el option
+    let option = document.createElement('option')
+    option.label =i.indicatorName
+    option.value=i.indicatorName
+    val=(Object.keys(i.data))  
+    indiSelect.appendChild(option)
+    // yearSelect.appendChild(option)
+
+  })
+}
+function setOptionsY(listY){
+  console.log(listY)
+  listY.forEach(val=>{
+    let optionY = document.createElement('option')
+    optionY.label =val
+    optionY.value=val      
+    yearSelect.appendChild(optionY)
+  
+  })
+}
+
+// 3.- listener
+select.addEventListener('change', onChange)
+// yearSelect.addEventListener('change', onChange1)
+indiSelect.addEventListener('change', onChange1)

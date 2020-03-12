bfb66dd1683c44fe558113db48129ae692d0735f
Fernanda de la Peña ☆ <kapirupia@gmail.com>
2020-03-12 12:27:11 -0600

    cambios

--- src/data.js
+++ src/data.js
@@ -1,3 +1,1 @@
-fetch('./data/worldbank/worldbank.json')
-.then(response=>response.json())
-.then(data=>console.log(data.PER.indicators));
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,82 @@
-import { example } from './data.js';
+import data from './data/worldbank/worldbank.js'
+//console.log(data.PER.indicators[9].data)
+//nodos
 
-console.log(example);
+import yearFilter from './data.js';
 
+let countrySelect = document.querySelector('#countrySelect')
+let indiSelect = document.querySelector('#indicatorSelect')
+let yearSelect = document.querySelector('#yearSelect')
+
+//variables globales
+let country
+let indicator
+let year
+let findIndicator
+let percents
+let percent
+let test
+
+let years = Object.keys(data.PER.indicators[0].data);
+let porcentajes = Object.values(data.PER.indicators[0].data);
+
+//funciones
+function onChange(e) {
+  let value = e.target.value
+  country = value
+  let indi = data[country].indicators
+  setOptions(indi)
+  console.log(country)
+}
+
+function setOptions(list) {
+  list.forEach(i=>{
+    let option = document.createElement('option')
+    option.value = i.indicatorName
+    option.label = i.indicatorName
+    indiSelect.appendChild(option)
+  })
+}
+
+function indiOnChange(e){
+let value = e.target.value
+indicator = value
+let findIndicator = data[country].indicators.find(obj=>obj.indicatorName === indicator)
+let years = Object.keys(findIndicator.data)
+percents = Object.values(findIndicator.data)
+setYearOptions(years)
+console.log(indicator)
+console.log(percents)
+}
+
+function setYearOptions(list) {
+  list.forEach(i=>{
+    let option = document.createElement('option')
+    option.value = i
+    option.label = i
+    yearSelect.appendChild(option)
+  })
+}
+
+function yearOnChange(e){
+  return e.target.value;
+/  // years.forEach((item) => {
+  //   if(item === year) {
+  //     console.log(item)
+  //     porcentajes.forEach((i) => {
+  //       console.log(i)
+  //     })
+  //   }
+  // })
+
+  //console.log(year)
+}
+
+yearFilter(yearOnChange)
+console.log(porcentajes)
+console.log(years)
+
+//listeners
+countrySelect.addEventListener('change', onChange)
+indiSelect.addEventListener('change', indiOnChange)
+yearSelect.addEventListener('change', yearOnChange)

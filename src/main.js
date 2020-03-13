9776fc4e5bf85c34a9830fca52932c195de68f1d
Fernanda de la Peña ☆ <kapirupia@gmail.com>
2020-03-13 14:13:19 -0600

    filtro por país, indicador y año

--- src/main.js
+++ src/main.js
@@ -1,25 +1,20 @@
-<<<<<<< HEAD
 import data from './data/worldbank/worldbank.js'
-//console.log(data.PER.indicators[9].data)
+
 //nodos
-
-import yearFilter from './data.js';
-
 let countrySelect = document.querySelector('#countrySelect')
 let indiSelect = document.querySelector('#indicatorSelect')
 let yearSelect = document.querySelector('#yearSelect')
+let sortDirection = false
+let tableData = []
+let tableBody = document.querySelector('#tableData')
+let tableBodyP = document.querySelector('#tableDataP')
 
 //variables globales
 let country
 let indicator
 let year
 let findIndicator
-let percents
 let percent
-let test
-
-let years = Object.keys(data.PER.indicators[0].data);
-let porcentajes = Object.values(data.PER.indicators[0].data);
 
 //funciones
 function onChange(e) {
@@ -35,51 +30,50 @@ function setOptions(list) {
     let option = document.createElement('option')
     option.value = i.indicatorName
     option.label = i.indicatorName
-=======
-// 1.- importamos la Data
-import data from './data/worldbank/worldbank.js'
-
-//5.- Guardamos en variables globales
-let country
-
-// 2.- selectores (nodos)
-let select = document.querySelector('#cText')
-let indiSelect = document.querySelector('#indicator')
-
-//4.- funciones callback
-function onChange(event){
-  let value = event.target.value
-  country = value
-  let indi = data[value].indicators
-  console.log(indi)
-  setOptions(indi)
-}
-
-//6.- funcion para colocar options en el select de indicator
-function setOptions(list){
-  list.forEach(i=>{
-    //a.- creamos el option
-    let option = document.createElement('option')
-    option.label = i.indicatorName
-    option.value=i.indicatorName
->>>>>>> d5fe10246d7b7a3e49874976950ce087dd2a001a
     indiSelect.appendChild(option)
   })
 }
 
-<<<<<<< HEAD
 function indiOnChange(e){
 let value = e.target.value
 indicator = value
-let findIndicator = data[country].indicators.find(obj=>obj.indicatorName === indicator)
+findIndicator = data[country].indicators.find(obj=>obj.indicatorName === indicator)
 let years = Object.keys(findIndicator.data)
-percents = Object.values(findIndicator.data)
-setYearOptions(years)
+let percent = Object.values(findIndicator.data)
+//let percents = Object.keys(findIndicator.data)
+//setYearOptions(years)
+//console.log(years)
+loadYears(years)
+loadPercent(percent)
 console.log(indicator)
-console.log(percents)
 }
 
-function setYearOptions(list) {
+function loadYears (list) {
+list.forEach(i=>{
+  let tr = document.createElement('TR')
+  tr.innerText = i
+  tableBody.appendChild(tr)
+  console.log(i)
+})
+}
+
+function loadPercent (list) {
+list.forEach(i=>{
+  let tr = document.createElement('TR')
+  tr.innerText = i
+  tableBodyP.appendChild(tr)
+  console.log(i)
+
+  if(i==""){
+    let tr = document.createElement('TR')
+    tr.innerText = '----'
+    tableBodyP.appendChild(tr)
+    console.log(i)
+  }
+})
+}
+
+/*function setYearOptions(list) {
   list.forEach(i=>{
     let option = document.createElement('option')
     option.value = i
@@ -89,28 +83,14 @@ function setYearOptions(list) {
 }
 
 function yearOnChange(e){
-  return e.target.value;
-/  // years.forEach((item) => {
-  //   if(item === year) {
-  //     console.log(item)
-  //     porcentajes.forEach((i) => {
-  //       console.log(i)
-  //     })
-  //   }
-  // })
-
-  //console.log(year)
+  let value = e.target.value
+  year = value
+  console.log(year)
 }
-
-yearFilter(yearOnChange)
-console.log(porcentajes)
-console.log(years)
+*/
 
 //listeners
 countrySelect.addEventListener('change', onChange)
 indiSelect.addEventListener('change', indiOnChange)
-yearSelect.addEventListener('change', yearOnChange)
-=======
-// 3.- listener
-select.addEventListener('change', onChange)
->>>>>>> d5fe10246d7b7a3e49874976950ce087dd2a001a
+//indiSelect.addEventListener('change', printRows)
+//yearSelect.addEventListener('change', yearOnChange)
